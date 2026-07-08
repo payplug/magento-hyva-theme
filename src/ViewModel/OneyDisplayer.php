5d10006d32443d96208b2cf508528e190601bd49
Frédéric TISSOT <contact@espritdev.fr>
2026-07-08 11:52:29 +0200

    MAG-791: Remove useless preference for Oney simulation controller class + move hardcoded route to ViewModel

--- src/ViewModel/OneyDisplayer.php
+++ src/ViewModel/OneyDisplayer.php
@@ -9,18 +9,45 @@ declare(strict_types=1);
 
 namespace Payplug\PaymentsHyvaTheme\ViewModel;
 
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Payplug\Payments\Helper\Oney;
 
 class OneyDisplayer implements ArgumentInterface
 {
+    private const ONEY_SIMULATION_ROUTE = 'hyva_checkout_payplug/oney/simulation';
+
+    /**
+     * @param Oney $oney
+     * @param UrlInterface $urlBuilder
+     */
     public function __construct(
-        private Oney $oney
+        private readonly Oney $oney,
+        private readonly UrlInterface $urlBuilder
     ) {
     }
 
+    /**
+     * Can display Oney
+     *
+     * @return bool
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
     public function canDisplayOney(): bool
     {
         return $this->oney->canDisplayOney();
+    }
+
+    /**
+     * Get Oney Simulation Url
+     *
+     * @return string
+     */
+    public function getOneySimulationUrl(): string
+    {
+        return $this->urlBuilder->getUrl(self::ONEY_SIMULATION_ROUTE);
     }
 }
